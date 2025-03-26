06ab03c971dfabe2bc297643240bc4ea794da954
zaya242 <isiomajadesimi@gmail.com>
2025-03-26 17:37:44 +0100

    activity2

---
+++ activity2.py
@@ -0,0 +1,16 @@
+
+def sum(n):
+    return n*(n+1)/2
+
+print(sum(25))
+
+arr=[10,20,16,35,66]
+
+def arraysum(a):
+    sum=0
+    for i in a:
+        sum=sum+1
+    return sum
+
+print(arraysum(arr))    
+
